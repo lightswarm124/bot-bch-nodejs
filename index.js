cba1cfa83f48a1a60ae3671fbe4cfcfe427fd874
lightswarm124 <lightswarm124@users.noreply.github.com>
2021-11-14 20:41:53 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const BCHJS = require("@psf/bch-js");
+let bchjs = new BCHJS();
+
+const SEC = 1000;
+
+async function latestBlockInfo() {
+  let runApp = setInterval(async () => {
+    try {
+      let getBestBlockHash = await bchjs.Blockchain.getBestBlockHash()
+        .then(async (hash) => {
+          let data = await bchjs.Blockchain.getBlock(hash);
+          delete data.tx;
+          return data;
+        }).catch(err =>{
+          return err
+        });
+      console.log(getBestBlockHash);
+    } catch (err) {
+      console.log(err);
+    }
+  }, 60 * SEC);
+}
+
+latestBlockInfo();
